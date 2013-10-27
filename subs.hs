88542c82b5b85c9d083f490fd5ef1890fa7a118c
forgit <aaaa111@mail.ru>
2013-10-27 14:26:27 +0400

    Add fundSubStr function

--- subs.hs
+++ subs.hs
@@ -3,5 +3,16 @@ Given: Two DNA strings s and t (each of length at most 1 kbp).
 Return: All locations of t as a substring of s.
 -}
 
+findSubStr [] t curpos pos = pos
+findSubStr s t curpos pos
+    | str == t  = findSubStr (tail s) t (curpos+1) (pos ++ [curpos])
+    | otherwise = findSubStr (tail s) t (curpos+1) (pos)
+    where
+        str = take (length t) s
+
+
+s = "GATATATGCATATACTT"
+t = "ATAT"
+
 main = do
-    print 0
+    print $ findSubStr s t 1 []
