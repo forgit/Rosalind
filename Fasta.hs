d134c12fdc526ced509499e5a67da7e791ee4d25
forgit <aaaa111@mail.ru>
2013-10-27 18:44:59 +0400

    Add Fasta module

---
+++ Fasta.hs
@@ -0,0 +1,11 @@
+module Fasta (splitStr, formTuple) where
+
+splitStr []     = ""
+splitStr [x]    = [x]
+splitStr [x,y]  = [x,y]
+splitStr (x:y:z:xs)
+    | x `elem` ['C','G','T','A'] && y == '\n' && z `elem` ['C','G','T','A'] = splitStr (x:z:xs)
+    | otherwise     = [x] ++ splitStr (y:z:xs)
+
+formTuple [] = []
+formTuple (x:y:xs) = [(x,y)] ++ formTuple (xs)
