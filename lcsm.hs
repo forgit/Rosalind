b3d4c793d381313db986e4e800e815feaad54a3e
forgit <aaaa111@mail.ru>
2013-10-28 22:47:10 +0400

    Add subtraction of two strings

--- lcsm.hs
+++ lcsm.hs
@@ -14,8 +14,42 @@ subStr' x yy@(y:ys)
     where
         inityy = init yy
 
-commonSubStr (x:xs) (y:ys)
-    | otherwise = 0
+
+commonSubs' a b
+    | length a < length b   = b `csubs` a
+    | otherwise       = a `csubs` b
+
+
+-- s - shorterst string
+test [] s =[]
+test (x:y:ys) s
+    | x=="1" && y=="1" = "" ++ test (ys)
+    | otherwise        = test ys
+
+{-
+    THE LARGEST SET OF ZEROS (IN THE SORTEST STRING) 
+    CORRESPONDS TO COMMON SUBSTRING
+-}
+-- bb is shorter
+aa `csubs` bb 
+    | na >= nb  = [bb `subs` (take nb aa)] ++ (tail aa `csubs` bb)
+    | otherwise = []
+    where 
+        nb = length bb
+        na = length aa
+
+-- if equal length
+[] `subs` [] = ""
+(a:as) `subs` (b:bs) = (a `sub` b) ++ as `subs` bs
+
+a `sub` b
+    | a==b = "0"
+    | otherwise = "1"
+
+
+
+--ss xx@(x:xs) yy@(y:ys)
+--    | y `isInfixOf` xx = [y] ++ ys
 
 main = do
     input <- readFile "rosalind_lcsm.txt"
@@ -25,4 +59,7 @@ main = do
         tmp'= removeRosalind tmp
     print tmp'
 
-    print $ subStr "ACGTACGT" "AACCGTATAAAACGT"
+    --print $ subStr "ACGTACGT" "AACCGTATAAAACGT"
+    print $ "AACCGTATAAAACGT" `csubs` "GTATACAACG"
+    print $ commonSubs' "GTATACAACG" "AACCGTATAAAACGT"
+
