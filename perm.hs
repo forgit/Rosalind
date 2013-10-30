55bf156b2110e8515084a32fc9db9dcfa985a933
forgit <aaaa111@mail.ru>
2013-10-30 21:09:41 +0400

    Add swap

--- perm.hs
+++ perm.hs
@@ -2,14 +2,15 @@ import Data.List
 
 perm n = [1..n]
 
-
-swap2 xx i j = xs ++ [z] ++ ys ++ [y] ++ zs
-    where
-        (xs,(y:ys))=splitAt i xx
-        (ts,(z:zs))=splitAt (j-1) xx
-
---let (ys,zs) = splitAt n xs   in   ys ++ [new_element] ++ zs
---let (ys,zs) = splitAt n xs   in   ys ++ (tail zs)
+--swaps element at position a with element at position b.
+swap xs a b
+    | a > b = swap xs b a
+    | a == b = xs
+    | a < b = 
+        let
+            (p1,p2) = splitAt a xs
+            (p3,p4) = splitAt (b-a-1) (tail p2)
+        in p1 ++ [xs!!b] ++ p3 ++ [xs!!a] ++ (tail p4)
 
 
 cartProd xs ys = [(x,y) | x <- xs, y <- ys]
@@ -39,4 +40,4 @@ main = do
     --print $ xs ++ [z] ++ ys
     print $  cartProd [1..3] [1..3]
     print $ aa [1..4]
-    print $ insert 3 [4,2,1]
+    print $ swap [1..4] 2 3
