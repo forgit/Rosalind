36f298ac6f3841aed4af425d07490ff33d6de636
forgit <aaaa111@mail.ru>
2013-11-01 21:29:21 +0400

    Solve iprb problem

--- iprb.hs
+++ iprb.hs
@@ -10,17 +10,38 @@ a dominant allele (and thus displaying the dominant phenotype).
 Assume that any two organisms can mate.
 -}
 
+{-
+ example: 2 2 2
+ 2 homozygous dominant (AA), 2 heterozygous (Aa), and 2 homozygous recessive (aa)
+ P(dominant) = (P(AA + AA) + P(AA + Aa) + P(Aa + Aa) + P(Aa + aa) + P(AA + aa)) / C(6,2)
+ = (1 + 4 + 1 * 0.75 + 4 * 0.5 + 4) / (6! / 2! * 4!)
+ = 0.78333
+-}
 
 -- fast check if length of list <=n
 fastCheck n = null . drop n
 
-k=2
-m=2
-n=2
-
 factorial n = product [1..n]
 
+c n k = factorial n / ((factorial k) * (factorial (n - k)))
 
+prob k m n = p
+    where 
+        s = [
+            k * (k - 1),        --AA, AA
+            k * m,              --AA, Aa
+            k * n,              --AA, aa
+            m * k,              --Aa, AA
+            m * (m - 1) * 0.75, --Aa, Aa
+            m * n * 0.5,        --Aa, aa
+            n * k,              --aa, AA
+            n * m * 0.5        --aa, Aa
+            ]
+        f = k + m + n
+        p = sum s / (f * (f - 1))
 
 main = do
-    print $ fastCheck 10 [1..100]
+    input <- readFile "rosalind_iprb.txt"
+    let 
+        (k:m:n:xs) = words input
+    print $ prob (read k) (read m) (read n)
