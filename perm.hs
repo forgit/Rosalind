496cefbce093123eba95eb4efed836c77e836771
forgit <aaaa111@mail.ru>
2013-11-05 21:32:47 +0400

    Solve perm problem

--- perm.hs
+++ perm.hs
@@ -1,39 +1,20 @@
-import Data.List
+import Data.List (delete)
 
-perm n = [1..n]
+-- adorable solution by Mgccl
+permute [] = [[]]
+permute xs = concat $ map (\x-> map (x:) (permute (delete x xs))) xs
 
---swaps element at position a with element at position b.
-swap xs a b
-    | a > b = swap xs b a
-    | a == b = xs
-    | a < b = 
-        let
-            (p1,p2) = splitAt a xs
-            (p3,p4) = splitAt (b-a-1) (tail p2)
-        in p1 ++ [xs!!b] ++ p3 ++ [xs!!a] ++ (tail p4)
+printColumn [] = putStrLn ""
+printColumn (x:xs) = do
+    putStrLn $ unwords $map show x
+    printColumn xs
 
 
-cartProd xs ys = [(x,y) | x <- xs, y <- ys]
-cp xs = [[x]++(filter (/=x) xs)|x<-xs]++[[x]++(filter (/=x) (reverse xs))|x<-xs]
-
-aa [] = []
-aa (x:xs) = [[x] ++ (filter (/=x) xs)] ++ aa xs
-
-
-xor a b = (a+b) `mod` 2
-
-del (x:xs) = xs++[x]
+n = 4    
 
 main = do
-    print $ [1..5]
-    --print $ swap2 [1..5] 2 3
-    let
-        i = 2
-        j = 4
-        xx = [1..5]
-        (xs,(y:ys))=splitAt (i-1) xx
-        (ts,(z:zs))=splitAt (j-1) xx
-    --print $ xs ++ [z] ++ ys
-    print $  cartProd [1..3] [1..3]
-    print $ aa [1..4]
-    print $ swap [1..4] 2 3
+    let l = [1..n]
+    let p = permute l
+    print $ product l
+    printColumn p
+
