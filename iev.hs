75b7c3dfc21ae01ed97b8e4f55a7c8992b689838
forgit <aaaa111@mail.ru>
2013-11-10 12:13:15 +0400

    Solve iev problem

---
+++ iev.hs
@@ -0,0 +1,32 @@
+{-
+Given: Six positive integers, each of which does not exceed 20,000. 
+The integers correspond to the number of couples in a population 
+possessing each genotype pairing for a given factor. 
+In order, the six given integers represent the number of 
+couples having the following genotypes:
+
+AA-AA
+AA-Aa
+AA-aa
+Aa-Aa
+Aa-aa
+aa-aa
+
+Return: The expected number of offspring displaying 
+the dominant phenotype in the next generation, 
+under the assumption that every couple has exactly two offspring.
+-}
+
+
+--    Parents:
+--  * AA - AA -> 1
+--  * AA - Aa -> 1
+--  * AA - aa -> 1
+--  * Aa - Aa -> 3/4
+--  * Aa - aa -> 1/2
+--  * aa - aa -> 0
+ 
+
+main = readFile "rosalind_iev.txt" >>= 
+    print . (*2) . sum . (zipWith (*) prob) . (map read) . words
+        where prob = [1, 1, 1, 3/4, 1/2, 0]
