45c957a4dfe31a3b3ac24b983cf737f34e6b2c9b
forgit <aaaa111@mail.ru>
2013-11-21 21:29:35 +0400

    Solve pper problem

--- pper.hs
+++ pper.hs
@@ -1,6 +1,11 @@
---factorial :: Integer -> Integer
-factorial n = product [1..n] `mod` 1000000
+{-
+Given: Positive integers n and k such that 100≥n>0 and 10≥k>0.
 
-main = do
+Return: The total number of partial permutations P(n,k), modulo 1,000,000.
+-}
 
-    print $ (fromIntegral $ factorial 21) / fromIntegral ((factorial 7) * (factorial 14))
+p :: Integer -> Integer -> Integer
+p n k = product [(n-k+1)..n] `mod` 1000000
+
+main = readFile "rosalind_pper.txt" >>=
+    print . foldr1 (p) . map (\x -> read x :: Integer) . words
