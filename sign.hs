1e1a1342267de15ad096e49b6e6d3e320d623ab5
forgit <aaaa111@mail.ru>
2013-11-28 18:09:37 +0400

    Solve sign problem

--- sign.hs
+++ sign.hs
@@ -3,10 +3,19 @@ import Data.List
 permute [] = [[]]
 permute xs = concat $ map (\x-> map (x:) (permute (delete x xs))) xs
 
+groupN [] _ = []
+groupN xs n = a : groupN b n 
+    where (a,b) = splitAt n xs
+
+sign n = nub $ map (\(x,y) -> y) $ filter (\(x,y) -> not x) $ map (\xs -> (False `elem` (map (\ss -> length ss == 1) $ group . sort$ map abs xs),xs)) $ aa 
+    where 
+        aa = (\xs -> groupN xs n) $ concat $ permutations $ [-n..n]\\[0]
+
+printA xs = mapM_ (putStrLn . unwords . map show) xs
+
+
 main = do
---    print $ length [(x,y) | x<-[1,2,-1,-2], y<-[-1,-2,1,2], x/=y, x/=(-y)]
     let n = 3
-    let t = map (\xs -> map (negate) xs) $ permute [1..n]
-    let a = permute [1..n]
---    print $ concat $ [[x,y]| x<-t,y<-a, (abs x)/=(abs y)]
-    print $ concat $ [[x,y]| x<-t,y<-a, x/=(y)] -- bad
+        r = sign n
+    print $ length r
+    printA r
